6304759c3b2c5257236e236963997b6de233877c
imaz <neko.daisuki.070109@gmail.com>
2011-11-06 02:48:21 +0900

    Implement fibonacci

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,16 @@
 require 'rspec'
 
+def fibonacci(count)
+	fib = 1
+	before = 0
+	for i in 1..count do
+		fib += before
+		before = fib - before
+		p fib
+	end
+	fib
+end
+
 describe "fibonacci" do
 	it "returns 3 if given 4" do
 		fibonacci(4).should == 3
