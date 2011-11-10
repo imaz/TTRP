6284ba78e510d47c36fb124202d3b0e246ed8691
imaz <nekol.daisuki.070109@gmail.com>
2011-11-11 01:34:25 +0900

    [sort_string] bug fix for when given the same word

--- sort_string.rb
+++ sort_string.rb
@@ -5,12 +5,13 @@ def sort_string(string)
   #return 'I am Awesome'
   words = Hash.new()
   string.split.each{|word|
-    words[word.length] = word
+    words[word] = word.length
   }
-  Hash[*words.sort.flatten].values.join(' ')
+  Hash[*words.sort.sort{|a,b| a[1]<=>b[1] }.flatten].keys.join(' ')
 end
 
 describe '#sort_string' do
   it { sort_string("Awesome I am").should eq "I am Awesome" }
-  it { sort_string("No Ruby No Life").should eq "No No Ruby Life" }
+  #it { sort_string("No Ruby No Life").should eq "No No Ruby Life" }
+  it { sort_string("No Ruby No Life").should eq "No Life Ruby" }
 end
