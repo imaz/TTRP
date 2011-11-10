e51f3dc2e5b5490d704541819f38608e3f64ee64
imaz <nekol.daisuki.070109@gmail.com>
2011-11-11 01:14:28 +0900

    [fibonacci] replace tab by whitespace

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,18 +1,18 @@
 require 'rspec'
 
 def fibonacci(count)
-	fib = 1
-	before = 0
-	for i in 1...count do
-		fib += before
-		before = fib - before
-	end
-	fib
+  fib = 1
+  before = 0
+  for i in 1...count do
+    fib += before
+    before = fib - before
+  end
+  fib
 end
 
 describe "fibonacci" do
-	it "returns 3 if given 4" do
-		fibonacci(4).should == 3
-	end
+  it "returns 3 if given 4" do
+    fibonacci(4).should == 3
+  end
 end
 
