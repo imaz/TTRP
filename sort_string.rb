db9ddf01cceb9011ccc7e5ee38e587f23680147b
imaz <nekol.daisuki.070109@gmail.com>
2011-11-11 15:11:23 +0900

    [sort_string] ワンライナーわかったお！！

--- sort_string.rb
+++ sort_string.rb
@@ -1,16 +1,10 @@
 require 'rspec'
 
 def sort_string(string)
-  #given 'Awesome I am'
-  #return 'I am Awesome'
-  words = Hash.new()
-  string.split.each{|word|
-    words[word] = word.length
-  }
-  Hash[*words.sort.sort{|a,b| a[1]<=>b[1] }.flatten].keys.join(' ')
+  string.split.sort.sort{|a,b| a.length<=>b.length}.join(' ')
 end
 
 describe '#sort_string' do
   it { sort_string("Awesome I am").should eq "I am Awesome" }
-  it { sort_string("No Ruby No Life").should eq "No Life Ruby" }
+  it { sort_string("No Ruby No Life").should eq "No No Life Ruby" }
 end
