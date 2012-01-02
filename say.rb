7c1097ae984bf98656f6d4ddc5d7618705deb19f
imaz <neko.daisuki.070109@gmail.com>
2012-01-03 02:14:29 +0900

    [say] ランダムボイス実装

--- say.rb
+++ say.rb
@@ -1,7 +1,12 @@
 def say(word,n)
   n.times do
-    `say #{word}`
+    `say -v #{voice()} #{word}`
   end
 end
 
-
+def voice()
+  voices = Dir.entries("/System/Library/Speech/Voices/")
+  voices.shift(2)
+  voices.map!{|v| v.sub(/.SpeechVoice/,'') }
+  voices.sample
+end
