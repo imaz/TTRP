c1af8a8e25a73b700fbbe6e4e48061eaff2fe4d7
IMAZU Asami <neko.daisuki.070109@gmail.com>
2012-01-22 21:10:51 +0900

    [tree] 罫線を使ったtreeを構築！〆ができていない

--- tree.rb
+++ tree.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# coding : UTF-8
 
 def tree
   gitFiles = `git ls-files`.split("\n")
@@ -7,9 +8,14 @@ def tree
 end
 
 def make_tree(gitFiles)
-  tree=[]
+  tree = []
+  tree << "."
   gitFiles.each{|file|
-    file.each_with_index.map{|f,i| tree << ("  "*i)+f}
+    file.each_with_index.map{|f,i|
+      tr = i==0 ? "├" : "│"
+      ee = i==0 ? "── " : " ├── "
+      tree << (tr + "  "*i + ee)+f
+    }
   }
   tree.uniq.map{|key|p key}
 end
