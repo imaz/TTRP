4749adc5e531272c8bf6237cedc13cd5f2aca473
IMAZU Asami <neko.daisuki.070109@gmail.com>
2015-12-05 14:19:59 +0900

    [refactor_me_if_else] クラス化

--- refactor_me_if_else.rb
+++ refactor_me_if_else.rb
@@ -1,53 +1,55 @@
 require 'rspec'
 
-def age_span(age)
-  raise if age < 0
+class Age
+  def self.span(age)
+    raise if age < 0
 
-  case age
-  when 0..2
-    'baby'
-  when 3..6
-    'little child'
-  when 7..12
-    'child'
-  when 13..18
-    'youth'
-  else
-    'adult'
+    case age
+    when 0..2
+      'baby'
+    when 3..6
+      'little child'
+    when 7..12
+      'child'
+    when 13..18
+      'youth'
+    else
+      'adult'
+    end
   end
 end
 
 describe do
   it 'age がマイナス値の場合、例外を発生させること' do
-    expect{age_span(-1)}.to raise_error
+    expect{Age.span(-1)}.to raise_error
   end
 
   it do
     (0..2).each do |age|
-      expect(age_span(age)).to eq 'baby'
+      expect(Age.span(age)).to eq 'baby'
     end
   end
 
   it do
     (3..6).each do |age|
-      expect(age_span(age)).to eq 'little child'
+      expect(Age.span(age)).to eq 'little child'
     end
   end
 
   it do
     (7..12).each do |age|
-      expect(age_span(age)).to eq 'child'
+      expect(Age.span(age)).to eq 'child'
     end
   end
 
   it do
     (13..18).each do |age|
-      expect(age_span(age)).to eq 'youth'
+      expect(Age.span(age)).to eq 'youth'
     end
   end
 
   it '19歳以上の場合、adult を返すこと' do
-    expect(age_span(19)).to eq 'adult'
-    expect(age_span(100)).to eq 'adult'
+    expect(Age.span(19)).to eq 'adult'
+    expect(Age.span(100)).to eq 'adult'
   end
 end
