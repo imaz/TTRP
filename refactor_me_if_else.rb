5a9eed1e1e7fd59aacb447f2b5be012950c72aad
IMAZU Asami <neko.daisuki.070109@gmail.com>
2015-12-07 10:52:31 +0900

    [refactor_me_if_else] メソッドをよりコンパクトに

--- refactor_me_if_else.rb
+++ refactor_me_if_else.rb
@@ -2,28 +2,17 @@ require 'rspec'
 
 class Age
   AGE_SPAN = {
-    baby: 0..2,
-    little_child: 3..6,
-    child: 7..12,
-    youth: 13..18,
-    adult: 19..Float::INFINITY
+    'baby' => 0..2,
+    'little child' => 3..6,
+    'child' => 7..12,
+    'youth' => 13..18,
+    'adult' => 19..Float::INFINITY
   }
 
   def self.span(age)
     raise if age < 0
 
-    case age
-    when AGE_SPAN[:baby]
-      'baby'
-    when AGE_SPAN[:little_child]
-      'little child'
-    when AGE_SPAN[:child]
-      'child'
-    when AGE_SPAN[:youth]
-      'youth'
-    when AGE_SPAN[:adult]
-      'adult'
-    end
+    AGE_SPAN.find{|k, v| v.include? age }.first
   end
 end
 
