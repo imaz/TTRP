dd3678771a121e5e6984bdeda2862a5de2136425
IMAZU Asami <neko.daisuki.070109@gmail.com>
2015-12-12 14:32:46 +0900

    [refactor_me_sort_lists] サイズが違う配列を渡した時のテストを追加

--- refactor_me/sort_lists.rb
+++ refactor_me/sort_lists.rb
@@ -24,4 +24,9 @@ class TestSortLists < Minitest::Test
     data = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].shuffle
     assert_equal([['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h']], sort_lists(data[0..3], data[4..7]))
   end
+
+  def test_sort_lists_for_different_size_lists
+    data = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].shuffle
+    assert_equal([['a', 'b', 'c'], ['d', 'e', 'f', 'g', 'h']], sort_lists(data[0..2], data[3..7]))
+  end
 end
